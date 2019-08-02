4e7686737f3d69feb1dfde882b04b43411c16ece
Roeland Jago Douma <roeland@famdouma.nl>
2019-08-02 20:08:54 +0200

    Add new LoadAdditionalScriptsEvent
    This adds a new event that is in the new style of event dispatching.
    This should allow more lazy loading and better sepeartion of concerns
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ apps/files/lib/Listener/LegacyLoadAdditionalScriptsAdapter.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2019, Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Files\Listener;
+
+use OCA\Files\Event\LoadAdditionalScriptsEvent;
+use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\EventDispatcher\IEventListener;
+
+class LegacyLoadAdditionalScriptsAdapter implements IEventListener {
+
+	/** @var IEventDispatcher */
+	private $dispatcher;
+
+	public function __construct(IEventDispatcher $dispatcher) {
+		$this->dispatcher = $dispatcher;
+	}
+
+	public function handle(Event $event): void {
+		if (!($event instanceof LoadAdditionalScriptsEvent)) {
+			return;
+		}
+
+		$legacyEvent = new Event(null, ['hiddenFields' => []]);
+		$this->dispatcher->dispatch('OCA\Files::loadAdditionalScripts', $legacyEvent);
+
+		$hiddenFields = $legacyEvent->getArgument('hiddenFields');
+		foreach ($hiddenFields as $name => $value) {
+			$event->addHiddenField($name, $value);
+		}
+	}
+
+}
